f324e2d01ce6254dc305f7227985441ca3bbdbc5
andrewkledzik <andrew.kledzik@gmail.com>
2019-06-13 22:39:44 +0100

    Basic scatter example with marker. Added second plot.

--- Tutorial4.py
+++ Tutorial4.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 x = [1,2,3,4,5,6,7,8]
 y = [5,2,4,2,1,4,5,2]
 
+x2 = [8,7,6,5,4,3,2,1]
+y2 = [4,8,6,7,2,1,4,6]
 
 plt.scatter(x,y, label='skitscat', color='k', marker="d")
+plt.scatter(x2,y2, label='skitscat', color='k', marker="d")
 
 plt.xlabel('x')
 plt.ylabel('y')
